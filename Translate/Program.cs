a495d7385a607c29b2105bba4a0cd7762546ff5e
oktay.senkan <oktay.senkan@gmail.com>
2019-11-18 04:52:15 +0300

    Add project files.

---
+++ Translate/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using RestSharp;
+
+namespace Translate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TranslatedWord translatedWord = Request("administration");
+            Console.WriteLine(translatedWord.English + " => " +  string.Join(", ", translatedWord.Turkish));
+            Console.ReadLine();
+        }
+
+        public static TranslatedWord Request(string textToConvert)
+        {
+            var client = new RestClient("https://translate.yandex.net/api/v1.5/tr.json");
+            var request = new RestRequest("/translate");
+            request.AddParameter("lang", "en-tr");
+            request.AddParameter("text", textToConvert);
+            request.AddParameter("key", "trnsl.1.1.20191118T004713Z.a2faaa96b41ab88d.007291ac94c0d0a76888ab6ccf20b6a4a1ca0e1f");
+            request.AddHeader("Content-type", "application/x-www-form-urlencoded");
+
+            var response = client.Post(request);
+            var content = response.Content;
+            ResponseData responseData = JsonConvert.DeserializeObject<ResponseData>(content);
+            //var response = client.Post<ResponseData>(request);
+            TranslatedWord word = new TranslatedWord
+            {
+                English = textToConvert,
+                Turkish = responseData.Text
+            };
+
+            return word;
+        }
+    }
+}
---
+++ Translate/TranslatedWord.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Translate
+{
+    class TranslatedWord
+    {
+        public string English { get; set; }
+        public List<string> Turkish { get; set; }
+    }
+}
